5b471aa5c9bb48424acfbb45cf7e43c6b112499c
Gabriel Ramos <gabrielramosprincipal@gmail.com>
2015-04-19 22:54:52 -0300

    Versão 0.1
    criação do projeto em java

---
+++ PCS/src/pcs/PCS.java
@@ -0,0 +1,15 @@
+
+package pcs;
+import java.util.Scanner;
+public class PCS {
+    public static void main(String[] args) {
+         System.out.println("Hello");
+         System.out.println("Entre com seu nome");
+         Scanner sc = new Scanner(System.in);   
+         String nome;
+         nome = sc.next();
+         String nome2;
+        
+    }
+    
+}
