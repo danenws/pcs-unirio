0005e6610e8837dd4c6dc19c3f7e71e207702e6d
flippe <flipeguedes@gmail.com>
2015-06-01 04:29:12 -0300

    Links entre telas
    Foquei no link entre as telas da aplicação e na funcionalidade com os
    dados armazenados.

--- PCS/src/Telas/Dificuldade.java
+++ PCS/src/Telas/Dificuldade.java
@@ -9,16 +9,15 @@ package Telas;
  *
  * @author felipequintanilha
  */
-public class TelaPrincipal extends javax.swing.JFrame {
+public class Dificuldade extends javax.swing.JFrame {
 
     /**
-     * Creates new form TelaPrincipal
+     * Creates new form Dificuldade
      */
-    public TelaPrincipal() {
+    public Dificuldade() {
         initComponents();
-        this.setSize(640, 430);
+        this.setSize(550, 350);
         this.setResizable(false);
-        
     }
 
     /**
@@ -30,77 +29,56 @@ public class TelaPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel2 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("EnglishTale - Main Screen");
         getContentPane().setLayout(null);
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Telas/Logo.png"))); // NOI18N
-        jLabel2.setText("jLabel2");
+        jButton2.setFont(new java.awt.Font("Luminari", 0, 13)); // NOI18N
+        jButton2.setText("Capítulo I");
+        getContentPane().add(jButton2);
+        jButton2.setBounds(140, 160, 120, 80);
+
+        jButton3.setFont(new java.awt.Font("Luminari", 0, 13)); // NOI18N
+        jButton3.setText("Conclusão");
+        jButton3.setToolTipText("");
+        getContentPane().add(jButton3);
+        jButton3.setBounds(420, 160, 120, 80);
+
+        jButton4.setFont(new java.awt.Font("Luminari", 0, 13)); // NOI18N
+        jButton4.setText("Capítulo II");
+        getContentPane().add(jButton4);
+        jButton4.setBounds(280, 160, 120, 80);
+
+        jLabel2.setFont(new java.awt.Font("Luminari", 0, 24)); // NOI18N
+        jLabel2.setText("Em qual parte você está?");
         getContentPane().add(jLabel2);
-        jLabel2.setBounds(190, 0, 440, 100);
+        jLabel2.setBounds(20, 10, 310, 80);
 
         jButton1.setFont(new java.awt.Font("Luminari", 0, 13)); // NOI18N
-        jButton1.setText("Entrar");
+        jButton1.setText("Prefácio");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
         getContentPane().add(jButton1);
-        jButton1.setBounds(400, 150, 110, 40);
+        jButton1.setBounds(10, 160, 110, 80);
 
-        jButton2.setFont(new java.awt.Font("Luminari", 0, 13)); // NOI18N
-        jButton2.setText("Cadastrar-se");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton2);
-        jButton2.setBounds(400, 200, 110, 40);
-
-        jButton3.setFont(new java.awt.Font("Luminari", 0, 13)); // NOI18N
-        jButton3.setText("Sair");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton3);
-        jButton3.setBounds(540, 360, 80, 29);
-
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Telas/cadastro.jpg"))); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Telas/dif2.jpg"))); // NOI18N
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(0, 0, 640, 410);
+        jLabel1.setBounds(0, 10, 560, 310);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // Sair da aplicação (botão)
-        
-        System.exit(0);
-    }//GEN-LAST:event_jButton3ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // Ir para a tela de cadastro
-        
-        TelaCadastro telaCadastro = new TelaCadastro();
-        telaCadastro.setVisible(true);
-        //this.dispose();
-    }//GEN-LAST:event_jButton2ActionPerformed
-
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-    TelaUsuarios tela = new TelaUsuarios();
-    
-    tela.setVisible(true);
-    // TODO add your handling code here:
+        // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -120,20 +98,20 @@ public class TelaPrincipal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Dificuldade.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Dificuldade.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Dificuldade.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Dificuldade.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TelaPrincipal().setVisible(true);
+                new Dificuldade().setVisible(true);
             }
         });
     }
@@ -142,6 +120,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
